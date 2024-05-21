2cbeaf23c043eee3c854b8ba8f77a828cecc8879
Sijan Paudel <sijannpaudel@gmail.com>
2024-05-21 23:40:06 +0545

    Removed script and theme access

--- app/routes/app.installation.jsx
+++ app/routes/app.installation.jsx
@@ -12,38 +12,6 @@ export async function loader({ request }) {
         const { admin, session } = await authenticate.admin(request);
         const { shop } = session;
         let response = { shop };
-        if (!shop) return response;
-
-        const scriptsTags = await admin.rest.resources.ScriptTag.all({ session });
-        response.hasScript = scriptsTags.data.some(obj => obj.src === "https://host-scripts.onrender.com/shopify-views-script.js");
-
-        const themes = await admin.rest.resources.Theme.all({ session });
-        const currentTheme = themes.data.find(theme => theme.role === 'main');
-
-        if (currentTheme) {
-            try {
-                const productPage = await admin.rest.resources.Asset.all({
-                    session,
-                    theme_id: currentTheme.id,
-                    asset: { "key": "templates/product.json" },
-                });
-
-                if (productPage.data.length > 0) {
-                    const asset = productPage.data[0];
-                    response.productPage = asset;
-                    const assetValue = JSON.parse(asset.value);
-                    const sectionKeys = Object.keys(assetValue.sections);
-                    response.sectionId = sectionKeys[0];
-                    response.blockInstalled = JSON.stringify(assetValue.sections).includes("6b3abca8-2d33-43dd-a64e-90115c65c50d");
-                } else {
-                    response.productPage = null;
-                }
-            } catch (error) {
-                response.liquid = true
-                return response;
-            }
-        }
-
         return response;
     } catch (error) {
         return { error: error.message };
@@ -78,7 +46,7 @@ export async function action({ request }) {
 }
 
 export default function Installation() {
-    const { shop, hasScript, liquid = false, sectionId = '', blockInstalled = false } = useLoaderData();
+    const { shop } = useLoaderData();
     const actionData = useActionData();
     const success = actionData ? actionData.success : false;
     const liquidbanner = useRef(null);
@@ -86,20 +54,10 @@ export default function Installation() {
 
     const { state } = useNavigation();
     const pending = state === "submitting";
-    const installURL = `https://${shop}/admin/themes/current/editor?template=product&addAppBlockId=6b3abca8-2d33-43dd-a64e-90115c65c50d/views-count&target=sectionId:${sectionId}`;
+    const installURL = `https://${shop}/admin/themes/current/editor?template=product&addAppBlockId=6b3abca8-2d33-43dd-a64e-90115c65c50d/views-count&target=sectionId:main`;
 
-    useEffect(() => {
-        if (liquid) {
-            liquidbanner.current?.focus();
-
-        } else {
-            jsonbanner.current?.focus();
-        }
-
-    }, [])
 
     const storeData = { domain: shop }
-    const mostViewedBlockData = { isInstalled: false }
 
     return (
         <Page>
@@ -107,17 +65,15 @@ export default function Installation() {
                 <PageTitle title='Installation' />
                 {!shop && <Banner
                     title="Oh, no."
-                    action={{ content: 'Refresh Page', url: '/app/' }}
+                    action={{ content: 'Refresh Page', url: '/app/installation' }}
                     tone="critical"
                 >
                     <p>Something went wrong.</p>
                 </Banner>}
 
                 <BlockStack gap={"600"}>
-                    <ViewsCountInstall hasScript={hasScript} liquidbanner={liquidbanner} pending={pending} jsonbanner={jsonbanner} blockInstalled={blockInstalled} installURL={installURL} />
-
-
-                    <MostViewedSection storeData={storeData} blockData={mostViewedBlockData} />
+                    <ViewsCountInstall liquidbanner={liquidbanner} pending={pending} jsonbanner={jsonbanner} installURL={installURL} />
+                    <MostViewedSection storeData={storeData} />
                 </BlockStack>
             </BlockStack>
         </Page>
